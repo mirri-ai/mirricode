ad107d852c018d0287481e1bdc7f3b47672c8dca
mirri-ai <mirricode@163.com>
2026-07-08 11:28:19 +0000

    fix: clear pnpm deps hash in flake.nix to get correct one from CI

--- flake.nix
+++ flake.nix
@@ -151,7 +151,7 @@
               inherit (finalAttrs) pname version src pnpmWorkspaces;
               inherit pnpm;
               fetcherVersion = 3;
-              hash = "sha256-RPjCWL7NqDSKgpHGL16zPlUOfjWN2rkaDY/4GFAD8VA=";
+              hash = "";
             };
 
             nativeBuildInputs = [
